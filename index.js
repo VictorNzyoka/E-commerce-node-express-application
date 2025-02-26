9a2eb5aebae14f7c1a2508c260be7a90860e91de
Victor Nzyoka Mutunga <mutunga.victor21@students.dkut.ac.ke>
2025-02-26 09:06:21 +0300

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(cookieParser());
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(cors({
-    origin: 'victor-nzyoka-react-node-mongodb-e-commerce-application.vercel.app',
+    origin: 'https://victor-nzyoka-react-node-mongodb-e-commerce-application.vercel.app',
     credentials: true
 }))
 
